cd6a67daab6aca08a228aea466721707fc5c40de
Ben Bravo <bendc312@gmail.com>
2024-11-08 16:50:46 -0500

    SETUP FINISHED!

--- arrowride/src/main/java/com/arrowride/ArrowRide.java
+++ arrowride/src/main/java/com/arrowride/ArrowRide.java
@@ -4,4 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArrowRide extends JavaPlugin{
     
+    @Override
+    public void onLoad() {
+        getLogger().info("ArrowRide loaded and ready to use.");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("ArrowRide has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ArrowRide has been disabled.");
+    }
 }
